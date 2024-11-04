b27604e560e72d01c639f0c66e9608ecb07e647d
Hypnos999 <flama2204@gmail.com>
2024-11-04 16:25:25 +0000

    remote training

--- main.py
+++ main.py
@@ -1,41 +1,67 @@
 import os
-from abalone import pippo
+import tensorflow as tf
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler    
+
+#Import the data into a dataframe
+def model_run(
+    path_to_dataset: str,
+    model_dir: str,
+):
+    try:
+        df = pd.read_csv(path_to_dataset)
+
+        le = preprocessing.LabelEncoder()
+        le.fit(df['diagnosis'])
+        df['diagnosis'] = le.transform(df['diagnosis'])
+
+        X = df[df.columns[2:-1]]
+        y = df['diagnosis']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=101)
+
+        scaler = MinMaxScaler()
+        scaler.fit(X_train)
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Dense(30, activation='relu'))
+        model.add(tf.keras.layers.Dense(15, activation='relu'))
+        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+        model.compile(loss='binary_crossentropy', optimizer='adam')
+
+        model.fit(x=X_train, y=y_train, epochs=10, validation_data=(X_test, y_test))
+
+        losses = pd.DataFrame(model.history.history)
+        # losses.plot()
+        
+        model.save_weights(rf'{model_dir}/my_model_weights.weights.h5')
+
+        return 'Ha funzionato'
+    
+    except Exception as e:
+        return str(e)
 
 model_dir = os.environ['SM_MODEL_DIR']
 
 print('#####################################')
 
 input_dir = os.environ['SM_INPUT_DIR']
-
-
-def list_files_and_directories(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * level
-        print(f'{indent}{os.path.basename(root)}/')
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print(f'{subindent}{f}')
-            
-list_files_and_directories(input_dir)
-
-try:
-    pippo(path=rf"{input_dir}/data/training/abalone.data")
-except Exception as e:        
-    with open(model_dir + '/pippo_error.txt', 'w') as f:
-        f.write(str(e) + "\n")
+output = model_run(
+    rf"{input_dir}/data/training/cancer_data.csv",
+    model_dir
+)
     
 with open(model_dir + '/output_model.txt', 'w') as f:
-    f.write('Ciao sono il modello di ml')
+    f.write(output)
     
-output_dir = os.environ['SM_MODEL_DIR']
+output_dir = os.environ['SM_OUTPUT_DIR']
 with open(output_dir + '/output.txt', 'w') as f:
     f.write('Ciao sono i log del training')
-
-'''with open(rf"{input_dir}/data/training/train.csv", 'r') as fp:
-    lines = len(fp.readlines())
-    print('########################################################Total Number of lines:', lines)
-    with open(output_dir + '/output_lines.txt', 'w') as f:
-        f.write(f'Ciao le righe sono: {lines}')'''
-        
-        
