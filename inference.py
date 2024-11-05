4ff1a5ba923cfccb4af646a956577b3165a373a4
Hypnos999 <flama2204@gmail.com>
2024-11-05 16:21:30 +0000

    inference pt. 2

--- inference.py
+++ inference.py
@@ -17,13 +17,27 @@ def doit():
         
         b = [14.05,27.15,91.38,600.4,0.09929,0.1126,0.04462,0.04304,0.1537,0.06171,0.3645,1.492,2.888,29.84,0.007256,0.02678,0.02071,0.01626,0.0208,0.005304,15.3,33.17,100.2,706.7,0.1241,0.2264,0.1326,0.1048,0.225,0.08321]
         
-        model = models.load_model("my_model.keras")
-        predict_input = np.array([
+        model_dir = environ['SM_MODEL_DIR']
+        print(f"######## La model dir è: {model_dir}")
+        
+        model = models.load_model(f"{model_dir}/my_model.keras")
+        # model = models.load_model("my_model.keras")
+        
+        predict_input1 = np.array([
            b
         ])
-        predict_result = model.predict(predict_input)
+        predict_result1 = model.predict(predict_input1)
 
-        return json.dumps({"predict_result": predict_result.tolist()})
+        predict_input2 = np.array([
+           m
+        ])
+        predict_result2 = model.predict(predict_input2)
+
+        
+        return json.dumps({
+            "predict_result_1": predict_result1.tolist(),
+            "predict_result_2": predict_result2.tolist()
+        })
     
     except Exception as e:
         return str(e)
